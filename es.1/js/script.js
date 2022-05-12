e44645732eb6c969caf62eb1dfabd13b6693e032
Simone <cirino.simone99@gmail.com>
2022-05-12 16:47:52 +0200

    finito esercizio parola palindroma

---
+++ es.1/js/script.js
@@ -0,0 +1,25 @@
+/*
+    Parola Palidroma js
+*/
+function palindroma(parola){
+    let parolaUtenteInversa = '';
+    for (let i=1; i<=parola.length; i++){
+        parolaUtenteInversa += parola[parola.length-i]
+    }
+    if (parola === parolaUtenteInversa){
+        return 'La parola è palindroma';
+    } else {
+        return 'La parola non è palindroma';
+    }
+}
+
+
+const textUser = document.getElementById('parola-utente');
+const button = document.getElementById('invio');
+const risultato = document.getElementById('risultato');
+button.addEventListener('click',
+    function () {
+        let palindromia = palindroma(textUser.value);
+        risultato.innerText = palindromia;
+    }
+);
