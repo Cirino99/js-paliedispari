94b1a67c686a4b4fd27bb93b739f60b6b910b41c
Simone <cirino.simone99@gmail.com>
2022-05-12 17:26:03 +0200

    finito esercizio pari o dispari

---
+++ es.2/js/script.js
@@ -0,0 +1,46 @@
+/*
+    Pari o dispari js
+*/
+
+const descrizione = document.getElementById('descrizione');
+const numeroUtente = document.getElementById('numero-utente');
+const numeroPc = Math.floor(Math.random() * 5 + 1);
+const pariOdispariUtente = document.getElementById('pari-dispari');
+const risultato = document.getElementById('risultato');
+const button = document.getElementById('invio');
+button.addEventListener('click', 
+    function(){
+        let numeroUtenteValore = parseInt(numeroUtente.value);
+        const numeroValidoUtente = controlloNumero(numeroUtenteValore);
+        const numeroPc = Math.floor(Math.random() * 5 + 1);
+        if (numeroValidoUtente){
+            let risultatoPariODispari = pariOdispari(numeroUtenteValore,numeroPc);
+            if (pariOdispariUtente.value === risultatoPariODispari){
+                risultato.innerText = `Complimenti hai vinto il numero uscito è ${risultatoPariODispari}`;
+            } else {
+                risultato.innerText = `RIP hai perso il numero uscito è ${risultatoPariODispari}`;
+            }
+        } else {
+            descrizione.innerText = 'ATTENZIONE! il numero inserito deve essere compreso tra 1 e 5';
+        }
+    }
+);
+
+function controlloNumero(numero){
+    if(numero>0 && numero<6){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function pariOdispari(primoNumero,secondoNumero){
+    let numero = primoNumero + secondoNumero;
+    console.log(numero);
+    if(numero%2){
+        return 'Dispari';
+    } else {
+        return 'Pari';
+    }
+}
